2f72fb44a7b38c0d086b52811b97cd051ea88fd9
DESKTOP-43PFTP9\n4oah <n4oahdev@gmail.com>
2018-04-15 12:24:11 +0900

    no message

--- 프로그래머스/level3-멀리 뛰기/py/main.py
+++ 프로그래머스/level3-멀리 뛰기/py/main.py
@@ -1,21 +1,12 @@
-import math
-
-# 남은 시간
-TIME = 4
-# 남은 일의 작업량
-WORK_AMOUNT = [4, 3, 3]
+# 칸의 개수
+LINE = 4
 
 
-def noOvertime(n, works):
-    result = 0
-
-    while n > 0:
-        works[works.index(max(works))] -= 1
-        n -= 1
-
-    result = int(sum(math.pow(n, 2) for n in works))
-
-    return result
+def jumpCase(num):
+    answer = 0
 
 
-print(noOvertime(TIME, WORK_AMOUNT))
+    return answer
+
+
+print(jumpCase(LINE))
